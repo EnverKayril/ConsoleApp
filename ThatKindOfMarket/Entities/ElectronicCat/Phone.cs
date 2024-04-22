14e8cabd82c7a3b378aff5406d5ec330f6701af7
EnverKayril <eckayril@gmail.com>
2024-04-22 13:27:41 +0300

    New Project

---
+++ ThatKindOfMarket/Entities/ElectronicCat/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThatKindOfMarket.Entities.Abstracts;
+
+namespace ThatKindOfMarket.Entities.ElectronicCat
+{
+    public class Phone : Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int? Stock { get; set; }
+        public string? Description { get; set; }
+        public int ElectronicId { get; set; }
+        public virtual Electronic Electronic { get; set; }
+    }
+}
---
+++ ThatKindOfMarket/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThatKindOfMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
